0e8ea81388460e71a2fd9a06385bef7aa60b03fe
dgiancaspro <dave.giancaspro@gmail.com>
2024-12-24 10:17:12 -0500

    Started Day 3 Part 2

--- 2024/day_3/b/day_3b.go
+++ 2024/day_3/b/day_3b.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"strconv"
 )
 
 func main() {
+
 	enabled := true
-	ans := 0
+	//ans := 0
 	if len(os.Args) != 2 {
 		fmt.Println("[!] Input file needed")
 		os.Exit(1)
@@ -20,20 +20,33 @@ func main() {
 		fmt.Printf("[!] %s", err)
 		os.Exit(1)
 	}
-	m := regexp.MustCompile(`mul\(\d{1,3}\,\d{1,3}\)`)
+	//m := regexp.MustCompile(`mul\(\d{1,3}\,\d{1,3}\)`)
+	dont := regexp.MustCompile(`don\'t\(\)`)
+	do := regexp.MustCompile(`do\(\)`)
 	src := bufio.NewScanner(f)
+
 	for src.Scan() {
 		line := src.Text()
-		matchSlice := m.FindAllString(line, -1)
-		reInt := regexp.MustCompile(`\d{1,3}`)
-		for i := 0; i < len(matchSlice); i++ {
-			nums := reInt.FindAllString(matchSlice[i], -1)
-			num1, _ := strconv.Atoi(nums[0])
-			num2, _ := strconv.Atoi(nums[1])
-			ans += num1 * num2
-
+		matchdont := dont.FindAllString(line, -1)
+		if len(matchdont) != 0 && enabled {
+			enabled = false
+			fmt.Println(line)
+		}
+		matchdo := do.FindAllString(line, -1)
+		if len(matchdo) != 0 && !enabled {
+			enabled = true
 		}
 
+		// reInt := regexp.MustCompile(`\d{1,3}`)
+		// for i := 0; i < len(matchSlice); i++ {
+		// 	nums := reInt.FindAllString(matchSlice[i], -1)
+		// 	num1, _ := strconv.Atoi(nums[0])
+		// 	num2, _ := strconv.Atoi(nums[1])
+		// 	ans += num1 * num2
+
 	}
-	fmt.Println(ans)
+
 }
+
+//fmt.Println(ans)
+//}
