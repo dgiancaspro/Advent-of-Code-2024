4ae8547c903f5c31003af1c6e887668649651d87
dgiancaspro <dave.giancaspro@gmail.com>
2024-12-25 00:31:16 -0500

    Removed some troubleshooting Print Statements

--- 2024/day_3/b/day_3b.go
+++ 2024/day_3/b/day_3b.go
@@ -27,15 +27,8 @@ func main() {
 		reInt := regexp.MustCompile(`\d{1,3}`)
 		line := src.Text()
 		matched := matchRE.FindAllString(line, -1)
-		fmt.Println(matched)
-		fmt.Println(matched[102])
-		nums := reInt.FindAllString(matched[102], -1)
-		num1, _ := strconv.Atoi(nums[0])
-		num2, _ := strconv.Atoi(nums[1])
-		fmt.Println(num1, num2)
 		for i := 0; i < len(matched); i++ {
 			cmd := matched[i]
-			fmt.Println(i, cmd, enabled)
 			if cmd == "don't()" && enabled {
 				enabled = false
 			} else if cmd == "don't()" && !enabled {
@@ -46,12 +39,11 @@ func main() {
 				continue
 			} else if enabled {
 				nums := reInt.FindAllString(cmd, -1)
-				fmt.Println(nums)
 				num1, _ := strconv.Atoi(nums[0])
 				num2, _ := strconv.Atoi(nums[1])
 				ans += num1 * num2
 			}
 		}
 	}
-	fmt.Println(ans)
+	fmt.Printf("[+] The Answer is: %d", ans)
 }
