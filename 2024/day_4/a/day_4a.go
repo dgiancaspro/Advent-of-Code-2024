567d85c4217ee4d3ef6bee3a165bf14c4e3504a4
dgiancaspro <dave.giancaspro@gmail.com>
2024-12-29 06:11:58 -0500

    Backup of recent changes

--- 2024/day_4/a/day_4a.go
+++ 2024/day_4/a/day_4a.go
@@ -4,11 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"reflect"
+	"regexp"
 )
+
+func hSearch(line string) int {
+	// fmt.Println(line)
+	searchString := "XMAS"
+	reSearch := regexp.MustCompile(searchString)
+	lineMatch := reSearch.FindAllString(line, -1)
+	return len(lineMatch)
+}
+
+func reverse(line string) string {
+	revLine := ""
+	for i := len(line) - 1; i >= 0; i-- {
+		revLine += string(line[i])
+	}
+	// fmt.Println(line, revLine)
+	return revLine
+}
 
 func main() {
 	var dataSlice []string
-	searchString := "XMAS"
+	var answer int
 
 	if len(os.Args) != 2 {
 		fmt.Println("[!] Need a file name Please")
@@ -22,6 +41,15 @@ func main() {
 	for data.Scan() {
 		dataSlice = append(dataSlice, data.Text())
 	}
+	matrix := len(dataSlice[0])
+	// var dataMatrix [matrix][matrix]string
 	fmt.Println(dataSlice)
-	fmt.Println(searchString)
+	fmt.Println(reflect.TypeOf(matrix), matrix)
+	fmt.Printf("[+] Answer:%d\n", answer)
 }
+
+// for i := 0; i < len(dataSlice); i++ {
+// 	answer += hSearch(dataSlice[i])
+// 	revLine := reverse(dataSlice[i])
+// 	answer += hSearch(revLine)
+// } The Quick Brown fox jumped over the lazy dogs 76 +
